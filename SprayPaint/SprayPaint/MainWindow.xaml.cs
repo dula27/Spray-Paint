4daa12db5f4778238ac1e6a464f63ef8b9f94460
dula27 <dula@gwu.edu>
2022-06-12 18:47:09 -0400

    Added: Mainwindow layout to open or create a file

---
+++ SprayPaint/SprayPaint/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SprayPaint
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            /* Welcome styling */
+            // Set the font size
+            WelcomeHolder.FontSize = 30;
+            // Center allign the Welcome text
+            WelcomeHolder.HorizontalAlignment = HorizontalAlignment.Center;
+            WelcomeHolder.VerticalAlignment = VerticalAlignment.Center;
+            // Set the content
+            WelcomeHolder.Text = "Welcome to Spray Painter";
+
+            /* Creating/Loading styling */
+            // Set the font size
+            NewHolder.FontSize = 20;
+            LoadHolder.FontSize = 20;
+            // Center allign the text and button
+            NewHolder.HorizontalAlignment = HorizontalAlignment.Center;
+            LoadHolder.HorizontalAlignment = HorizontalAlignment.Center;
+            // Set the content
+            NewHolder.Text = "New file";
+            LoadHolder.Text = "Load file";
+            btnNew.Content = "Create";
+            btnLoad.Content = "Open";
+        }
+    }
+}

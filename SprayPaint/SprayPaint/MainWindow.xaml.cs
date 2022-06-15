7aa654ef04f389e335e073a07e9da357d1f06ac7
dula27 <dula@gwu.edu>
2022-06-14 22:22:37 -0400

    Added: New load window, Load dialogue box, InkCanvas

--- SprayPaint/SprayPaint/MainWindow.xaml.cs
+++ SprayPaint/SprayPaint/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -26,9 +27,11 @@ namespace SprayPaint
             /* Welcome styling */
             // Set the font size
             WelcomeHolder.FontSize = 30;
+
             // Center allign the Welcome text
             WelcomeHolder.HorizontalAlignment = HorizontalAlignment.Center;
             WelcomeHolder.VerticalAlignment = VerticalAlignment.Center;
+
             // Set the content
             WelcomeHolder.Text = "Welcome to Spray Painter";
 
@@ -36,12 +39,63 @@ namespace SprayPaint
             // Set the font size
             NewHolder.FontSize = 20;
             LoadHolder.FontSize = 20;
+
             // Center allign the text and button
             NewHolder.HorizontalAlignment = HorizontalAlignment.Center;
             LoadHolder.HorizontalAlignment = HorizontalAlignment.Center;
+
             // Set the content
             NewHolder.Text = "New file";
             LoadHolder.Text = "Load file";
+        }
+
+        //Button function for load button
+        private void btnLoadClick(object sender, RoutedEventArgs e)
+        {
+            // Create new window object
+            Window1 LoadWindow = new Window1();
+
+            // Configure open file dialog box
+            var dialog = new Microsoft.Win32.OpenFileDialog();
+            dialog.Title = "Select a picture";
+            dialog.Filter = "All supported graphics|*.jpg;*.jpeg;*.png|" +
+              "JPEG (*.jpg;*.jpeg)|*.jpg;*.jpeg|" +
+              "Portable Network Graphic (*.png)|*.png";
+
+            // Show open file dialog box
+            bool? result = dialog.ShowDialog();
+
+            // Process open file dialog box results
+            if (result == true)
+            {
+                // Open document
+                string filename = dialog.FileName;
+                BitmapImage loadedImg = new BitmapImage(new Uri(filename));
+
+                // Load source into xaml
+                LoadWindow.imgPhoto.Source = loadedImg;
+                LoadWindow.Title = "Spray Painter - Edit - " + filename;
+
+                /* Idea is to see if loaded Image size is greater than our current screen size, 
+                 * if yes then it should auto rescale the window size. I do not want to do that
+                 * for smaller images because then the brush ribbon would not fit. However, bitmap
+                 * images cannot figure out size at runtime, so need async thread to decode size at
+                 * runtime so this does not work. */
+                
+                if (loadedImg.PixelHeight > 700 || loadedImg.PixelWidth > 1000)
+                {
+                    // Scale the content to change window size automatically to fill
+                    this.SizeToContent = SizeToContent.WidthAndHeight;
+                }
+
+                // Hide current window visibility
+                this.Visibility = Visibility.Hidden;
+            }
+            else
+            {
+                MessageBox.Show("Unable to load file, try again.", "Load error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            LoadWindow.Show();
         }
     }
 }
